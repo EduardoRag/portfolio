d47aad7f9a00b4bf65b9f52d63a12a8d34aa45e0
EduardoRag <eduardojarek66@gmail.com>
2023-08-02 12:52:23 -0300

    Feat: Created skills section

---
+++ src/components/Skills.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/skills.module.scss';
+
+function Skills() {
+    return (
+        <section>
+            <article className={styles.article}>
+                <h2 className='h2'>
+                    🤓 Skills - Experiências
+                </h2>
+            </article>
+        </section>
+    )
+}
+
+export default Skills;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
-import styles from '../styles/home.module.scss';
-import Header from '../components/Header';
 import AboutMe from '../components/AboutMe';
+import Header from '../components/Header';
 import Projects from '../components/Projects';
+import Skills from '../components/Skills';
+import styles from '../styles/home.module.scss';
 
 function Home() {
   return (
@@ -12,8 +13,9 @@ function Home() {
       <div className={styles.line} />
       <Projects />
       <div className={styles.line} />
+      <Skills />
+      <div className={styles.line} />
     </main>
   );
 }
-
 export default Home;
